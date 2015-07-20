4dae4ff153bef90a402222a64c0b1b6e67fa2d39
Oleksandr <oleksandr.kyetov@gmail.com>
2015-07-20 18:17:21 +0200

    1. Small TODO added

--- src/main/java/algorithms/sort/impl/MedianOfThreePivotPicker.java
+++ src/main/java/algorithms/sort/impl/MedianOfThreePivotPicker.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public class MedianOfThreePivotPicker<T extends Comparable<T>> implements PivotPicker<T> {
 
 	@Override
+	// TODO Is it really a good way to get median???
 	public int pick(T[] a, int start, int end) {
 		if (a[start].compareTo(a[(start + end - 1) / 2]) > 0 && a[start].compareTo(a[end - 1]) < 0
 			|| a[start].compareTo(a[(start + end - 1) / 2]) < 0 && a[start].compareTo(a[end - 1]) > 0) {
